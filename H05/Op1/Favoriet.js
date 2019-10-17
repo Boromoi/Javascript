1b55442666a04c17de82397ca7725f11b575e565
LiamSROC <Lvanslingerlandt@roc-dev.com>
2019-10-17 09:13:42 +0200

    Klaar met H05.

--- H05/Op1/Favoriet.js
+++ H05/Op1/Favoriet.js
@@ -1,11 +1,40 @@
 pics = document.getElementById("pics");
 createPicturesHolders();
+createPokemonImages();
 
 function createPicturesHolders() {
     for (var i = 0; i < 9; i++) {
         pictureHolder = document.createElement("div");
-        pictureHolder.className = "pokemonpicture";
+        pictureHolder.className = "picture-holder";
         pictureHolder.id = "picture-holder-" + i;
         pics.appendChild(pictureHolder);
     }
+}
+
+function createPokemonImages() {
+        pictureHolder = document.getElementsByClassName("picture-holder");
+    for (var i = 0; i < pictureHolder.length; i++) {
+        favoriet = document.createElement("div");
+        favoriet.className = "favoriet";
+        favoriet.id = "favoriet" + (i+1);
+         pokemonplaatje = document.createElement("img");
+         pokemonplaatje.src = "img/pokemon" + (i+1) + ".jpg";
+         pokemonplaatje.id = (i+1);
+         pokemonplaatje.addEventListener("click", function() {
+             maakFavoriet(this.id);
+         });
+         pictureHolder[i].appendChild(favoriet);
+         pictureHolder[i].appendChild(pokemonplaatje);
+    }
+}
+
+function maakFavoriet(id) {
+    notsofavoriet = document.getElementsByClassName("favoriet");
+
+    for (var i = 0; i < notsofavoriet.length; i++) {
+        notsofavoriet[i].style.backgroundImage = "none";
+    }
+
+    favoriet = document.getElementById("favoriet" + id);
+    favoriet.style.backgroundImage = "url('img/hart.png')"
 }
